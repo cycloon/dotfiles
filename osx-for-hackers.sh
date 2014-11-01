a5e3cc83ba2ea518dc80501a9158a92c75870fc4
Christian Gut <cycloon@is-root.org>
2014-11-01 11:09:49 +0100

    Remove computer name setting

--- osx-for-hackers.sh
+++ osx-for-hackers.sh
@@ -39,13 +39,13 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # General UI/UX
 ###############################################################################
 
-echo ""
-echo "Setting the hostname to $COMPUTER_NAME"
-
-sudo scutil --set ComputerName $COMPUTER_NAME
-sudo scutil --set HostName $COMPUTER_NAME
-sudo scutil --set LocalHostName $COMPUTER_NAME
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+#echo ""
+#echo "Setting the hostname to $COMPUTER_NAME"
+#
+#sudo scutil --set ComputerName $COMPUTER_NAME
+#sudo scutil --set HostName $COMPUTER_NAME
+#sudo scutil --set LocalHostName $COMPUTER_NAME
+#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
 
 echo ""
 echo "Setting dark theme of yosemite"
